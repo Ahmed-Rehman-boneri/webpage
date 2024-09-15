ebe39693b4db23146c5343788aee283443a369e1
Ahmed Rehman <ahmedrehman0010@gmail.com>
2024-09-15 17:04:51 +0500

    updates

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 const nav = document.querySelector(".nav"),
-  // searchIcon = document.querySelector("#searchIcon"),
-  navOpenBtn = document.querySelector(".navOpenBtn"),
-  navCloseBtn = document.querySelector(".navCloseBtn");
-
-// searchIcon.addEventListener("click", () => {
-//   nav.classList.toggle("openSearch");
-//   nav.classList.remove("openNav");
-//   if (nav.classList.contains("openSearch")) {
-//     return searchIcon.classList.replace("uil-search", "uil-times");
-//   }
-//   searchIcon.classList.replace("uil-times", "uil-search");
-// });
+    navOpenBtn = document.querySelector(".navOpenBtn"),
+    navCloseBtn = document.querySelector(".navCloseBtn");
 
 navOpenBtn.addEventListener("click", () => {
-  nav.classList.add("openNav");
-  // nav.classList.remove("openSearch");
-  // searchIcon.classList.replace("uil-times", "uil-search");
+    nav.classList.add("openNav");
+
 });
 navCloseBtn.addEventListener("click", () => {
-  nav.classList.remove("openNav");
+    nav.classList.remove("openNav");
 });
-
 
 
 
 // swiper for branding
-   
-   const swiper = new Swiper('.swiper', {
+
+const swiper = new Swiper('.swiper', {
     loop: true,
     pagination: {
         el: '.swiper-pagination',
@@ -46,10 +34,73 @@ navCloseBtn.addEventListener("click", () => {
             slidesPerView: 3,
         },
         1280: {
-          slidesPerView: 5,
-      },
+            slidesPerView: 5,
+        },
     },
 });
+
+
+// banner text swper
+
+
+const rotator = document.querySelector('.rotation-container');
+const rotationItems = rotator.querySelectorAll('.rotation-item');
+let activeIndex = 0;
+
+function updateRotation() {
+    rotationItems.forEach((item, index) => {
+        item.classList.remove('visible');
+        let angleOffset = index - activeIndex;
+        if (angleOffset < 0) angleOffset += rotationItems.length;
+        item.style.transform = `rotateY(${angleOffset * 120}deg) translateZ(300px)`;
+    });
+    rotationItems[activeIndex].classList.add('visible');
+}
+
+function rotateItems() {
+    activeIndex = (activeIndex + 1) % rotationItems.length;
+    updateRotation();
+}
+
+updateRotation();
+setInterval(rotateItems, 6000);
+
+
+// banner imgage swiper
+
+const carousel = document.querySelector('.carousel');
+const items = carousel.querySelectorAll('.carousel__item');
+let currentIndex = 0;
+
+function updateCarousel() {
+    items.forEach((item, index) => {
+        item.classList.remove('active');
+        let offset = index - currentIndex;
+        if (offset < 0) offset += items.length;
+        item.style.transform = `rotateY(${offset * 120}deg) translateZ(300px)`;
+    });
+    items[currentIndex].classList.add('active');
+}
+
+function rotateCarousel() {
+    currentIndex = (currentIndex + 1) % items.length;
+    updateCarousel();
+}
+
+updateCarousel();
+setInterval(rotateCarousel, 6000);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
